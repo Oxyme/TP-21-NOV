a7394cb33c879f8aa346af84921be6daa8ab3926
oxyme <theobeulque281200@gmail.com>
2025-11-21 13:35:13 +0100

    FinV2

--- Javascript/js.js
+++ Javascript/js.js
@@ -121,35 +121,35 @@ Matter.Events.on(engine, "beforeUpdate", () => {
 
 // DEUXIEME CANVA
 
-const canvas2 = document.getElementById('canvas2');
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerHeight;
+// const canvas2 = document.getElementById('canvas2');
+// canvas2.width = window.innerWidth;
+// canvas2.height = window.innerHeight;
 
-const engine2 = Matter.Engine.create();
+// const engine2 = Matter.Engine.create();
 
-const render2 = Matter.Render.create({
-  canvas: canvas2,
-  engine: engine2,
-  options: {
-    wireframes: false,
-    background: 'transparent' // pour voir le fond beige derrière
-  }
-});
+// const render2 = Matter.Render.create({
+//   canvas: canvas2,
+//   engine: engine2,
+//   options: {
+//     wireframes: false,
+//     background: 'transparent' // pour voir le fond beige derrière
+//   }
+// });
 
-// Exemple de bodies pour le second canvas
-const box2 = Matter.Bodies.circle(400, 100, 40, {
-  render: { fillStyle: '#F4A261' }
-});
+// // Exemple de bodies pour le second canvas
+// const box2 = Matter.Bodies.circle(400, 100, 40, {
+//   render: { fillStyle: '#F4A261' }
+// });
 
-Matter.World.add(engine2.world, [box2]);
+// Matter.World.add(engine2.world, [box2]);
 
-const runner2 = Matter.Runner.create();
-Matter.Runner.run(runner2, engine2); // <-- obligatoire
+// const runner2 = Matter.Runner.create();
+// Matter.Runner.run(runner2, engine2); // <-- obligatoire
 
-Matter.Render.run(render2);
+// Matter.Render.run(render2);
 
 
-window.addEventListener('resize', () => {
-  canvas.width = canvas2.width = window.innerWidth;
-  canvas.height = canvas2.height = window.innerHeight;
-});
+// window.addEventListener('resize', () => {
+//   canvas.width = canvas2.width = window.innerWidth;
+//   canvas.height = canvas2.height = window.innerHeight;
+// });
